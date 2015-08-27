5a77b97a94a9af35dcf39d70dbd8232849550912
deathstryker <deathstryker@avalon.avalon>
2015-08-27 20:56:14 +0200

    maj

--- pages/template/script.php
+++ pages/template/script.php
@@ -1,4 +1,8 @@
-
+<?php if (isset($script_js) && is_array($script_js)): ?>
+  <?php foreach ($script_js as $key => $value): ?>
+    <script src="/assets/js/<?= $value ?>"></script>
+  <?php endforeach; ?>
+<?php endif; ?>
 <script src="/assets/js/vendor/jquery-1.11.3.min.js"></script>
 <script src="/assets/js/vendor/modernizr-2.8.3.min.js"></script>
 <script src="/assets/js/bootstrap.min.js"></script>
