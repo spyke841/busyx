99b62c9609adf0041c215f38bdbd4b20b698b565
spyke841 <spyke841@gmail.com>
2015-08-29 11:19:44 +0200

    maj

--- pages/home.php
+++ pages/home.php
@@ -2,49 +2,90 @@
 $position = ['Accueil'];
 $stylesheet[] = 'font_home';
 $stylesheet[] = 'anim_accueil';
- ?>
+?>
 <div class="container">
   <h1 class="entete marge_null" style="margin:20px 0 20px 0;">Bienvenue sur la boutique BusyX</h1>
   <div class="row">
     <aside class="col-md-8">
       <!-- <h1 class="entete marge_null">Bienvenue sur la boutique <span class="logo_busyx"><span class="logo_b">B</span>usy<span class="logo_x txt-white bg_round"><b>X</b></span></span></h1> -->
       <div class="en_avant wrapper row">
-        <p>
-          Vous avez du trafic coquin et vous souhaitez rentabiliser vos visiteurs en leur proposant des produits adaptés à leurs envies ?<br><br>Professionnels de la vente par internet d'articles pour adultes, nous vous proposons de générer des revenus substantiels en associant nos savoirs faire.
+        <div class="col-md-12">
+          <p>
+            Vous avez du trafic coquin et vous souhaitez rentabiliser vos visiteurs en leur proposant des produits adaptés à leurs envies ?<br><br>Professionnels de la vente par internet d'articles pour adultes, nous vous proposons de générer des revenus substantiels en associant nos savoirs faire.
 
-          <h1 class="txt-center" style="color: #535353; padding: 0px; margin: 0px; font-weight:normal;">Les Boutiques C<span style="font-family: arial; font-weight: bold;">'</span>est Notre Metier</h1>
-          <ul>
-            <li>
-              <b>Des atouts incomparables pour vos visiteurs :</b>
-              <ul>
-                <li>Des produits et rayons exclusifs (chaussures, librairie, corsets traditionnels, latex, Fetish SM...)</li>
-                <li>Des prix de vente parmi les plus bas du marché</li>
-                <li>Un effort tout particulier sur la qualité de notre service clientèle.</li>
-              </ul>
-            </li>
-          </ul>
-          <hr>
-          <ul>
-            <li>
-              <b>Une technologie de pointe au service des webmasters :</b>
-              <ul>
-                <li>De nombreux marketings de sexshops et de boutiques en niche avec votre id en sous domaine</li>
-                <li>Des Marques Blanches simplifiées et évoluées installables sur votre sous domaine ou domaine.</li>
-                <li>Flux xml et RSS, URL rewriting proposés sur l'ensemble des boutiques.</li>
-                <li>Des Outils promos et statistiques évolués.</li>
-              </ul>
-            </li>
-          </ul>
-        </p>
+            <h1 class="txt-center" style="color: #535353; padding: 0px; margin: 0px; font-weight:normal;">Les Boutiques C<span style="font-family: arial; font-weight: bold;">'</span>est Notre Metier</h1>
+            <ul>
+              <li>
+                <b>Des atouts incomparables pour vos visiteurs :</b>
+                <ul>
+                  <li>Des produits et rayons exclusifs (chaussures, librairie, corsets traditionnels, latex, Fetish SM...)</li>
+                  <li>Des prix de vente parmi les plus bas du marché</li>
+                  <li>Un effort tout particulier sur la qualité de notre service clientèle.</li>
+                </ul>
+              </li>
+            </ul>
+            <hr>
+            <ul>
+              <li>
+                <b>Une technologie de pointe au service des webmasters :</b>
+                <ul>
+                  <li>De nombreux marketings de sexshops et de boutiques en niche avec votre id en sous domaine</li>
+                  <li>Des Marques Blanches simplifiées et évoluées installables sur votre sous domaine ou domaine.</li>
+                  <li>Flux xml et RSS, URL rewriting proposés sur l'ensemble des boutiques.</li>
+                  <li>Des Outils promos et statistiques évolués.</li>
+                </ul>
+              </li>
+            </ul>
+          </p>
+        </div>
       </div>
       <div class="en_avant wrapper row">
-        <div class="txt_maxi anim_mb">
-          <span class="full_mb">FULL MB</span>
-          <span class="embeed_anim">
-            <span class="t1">Créez les univers<br>qui vous ressemblent !</span>
-            <span class="t2">Découvrez les marques <br>blanches TOTALES</span>
-            <span class="t3">Appréciez les boutiques Sexy <br>100% PARAMÉTRABLES</span>
-          </span>
+        <div class="col-md-12">
+          <div class="txt_maxi anim_mb">
+            <span class="full_mb">FULL MB</span>
+            <span class="embeed_anim">
+              <span class="t1">Créez les univers<br>qui vous ressemblent !</span>
+              <span class="t2">Découvrez les marques <br>blanches TOTALES</span>
+              <span class="t3">Appréciez les boutiques Sexy <br>100% PARAMÉTRABLES</span>
+            </span>
+          </div>
+          <p>
+            Sexshop, boutique de
+            lingerie sexy ou encore de sextoys, c'est vous qui décidez!
+            <br>
+
+            Choisissez votre domaine ou sous domaine et nous l'hébergeons !<br>
+            Nommez votre boutique, définissez ses rayons et paramétrez
+            votre page principale à volonté.<br>
+            <br>
+            Utilisez des graphismes prêts à l'emplois ou vos logos et
+            feuilles de styles personnalisées.<br>
+
+            Préparez votre indexation en renommant vos catégories, vos
+            descriptifs, en utilisant vos métas et zones de texte.
+          </p>
+        </div>
+      </div>
+
+      <div class="en_avant wrapper row">
+        <div class="col-md-4">
+          <div class="center-block bg_orange txt-center txt-white">
+            <p>
+              <span class="txt-border-orange font-exo">Reversement à partir de</span><br>
+              <span class="txt-big txt-border-orange upper font-exo">25%</span><br>
+                <span class="txt-border-orange font-exo">du CA HT hors frais de port</span>
+            </p>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="center-block bg_orange txt-center txt-white">
+            <span class="txt-big txt-border-orange upper font-exo">ref</span>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="center-block bg_orange txt-center txt-white">
+            <span class="txt-big txt-border-orange upper font-exo">ref</span>
+          </div>
         </div>
       </div>
     </aside>
